1cba56c8f954448e51c7ae1a017b3feddab6d2ae
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-07-02 04:27:21 +0200

    jfltevzw: Remove uneeded call to jfltevzw-vendor-blobs
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/jfltevzw/jfltevzw-vendor.mk)
-
 ## device overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltevzw/overlay
 
